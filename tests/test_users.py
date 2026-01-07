833b5867c493517b1bdedad67d2cc3366ce1b024
Rost911 <rostshulym@gmail.com>
2026-01-07 19:48:09 +0100

    Маркировки Pytest

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,11 +1,13 @@
 from http import HTTPStatus
-
+import pytest
 from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.users import assert_create_user_response
 from tools.assertions.schema import validate_json_schema
 
+@pytest.mark.users
+@pytest.mark.regression
 def test_create_user():
     # Инициализируем API-клиент для работы с пользователями
     public_users_client = get_public_users_client()
