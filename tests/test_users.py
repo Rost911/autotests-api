c539e410662eff9dea23aec87e48b589984013b8
Rost911 <rostshulym@gmail.com>
2026-01-26 21:27:38 +0100

    Практика работы с pytest параметризацией

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,23 +1,23 @@
 from http import HTTPStatus
-from urllib import request
-
 import pytest
-
 from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import  PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
 from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.users import assert_create_user_response, assert_user, assert_get_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
 from tools.assertions.schema import validate_json_schema
+from tools.fakers import fake
 
+@pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):
+def test_create_user(email: str, public_users_client: PublicUsersClient):
     # Используем фикстуру API клиента
     # Удалили инициализацию API клиента из теста
     # Формируем тело запроса на создание пользователя
-    request = CreateUserRequestSchema()
+    email_domain = fake.email(domain=email)
+    request = CreateUserRequestSchema(email=email_domain)
     # Отправляем запрос на создание пользователя
     response = public_users_client.create_user_api(request)
     # Инициализируем модель ответа на основе полученного JSON в ответе
@@ -42,6 +42,5 @@ def test_get_user_me(private_users_client: PrivateUsersClient, function_user: Us
         get_user_response=response_data,
         create_user_response=function_user.response
     )
-
 
 
