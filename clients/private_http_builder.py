d7363694fe8c53ac059c36dfe1fd8ec814a2c4ff
Rost911 <rostshulym@gmail.com>
2026-01-27 21:54:32 +0100

    Рефакторинг и подготовка к написанию API автотестов

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -1,25 +1,26 @@
+from functools import lru_cache  # Импортируем функцию для кеширования
+
 from httpx import Client
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel
 
-from clients.authentication.authentication_client import get_authentication_client, LoginRequestSchema
+from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_schema import LoginRequestSchema
 
 
-class AuthenticationUserSchema(BaseModel):
+class AuthenticationUserSchema(BaseModel, frozen=True):  # Добавили параметр frozen=True
     email: str
     password: str
 
-def get_private_http_client(user: AuthenticationUserSchema) -> Client:
-    """
-    Функция создаёт экземпляр httpx.Client с аутентификацией пользователя.
 
-    :param user: Объект AuthenticationUserSchema с email и паролем пользователя.
-    :return: Готовый к использованию объект httpx.Client с установленным заголовком Authorization.
-    """
+@lru_cache(maxsize=None)  # Кешируем возвращаемое значение
+def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     authentication_client = get_authentication_client()
+
     login_request = LoginRequestSchema(email=user.email, password=user.password)
     login_response = authentication_client.login(login_request)
+
     return Client(
         timeout=100,
-        base_url="http://localhost:8000/",
-        headers={"Authorization": f"Bearer {login_response.token.access_token}"},
+        base_url="http://localhost:8000",
+        headers={"Authorization": f"Bearer {login_response.token.access_token}"}
     )
