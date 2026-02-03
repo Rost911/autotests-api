1e52eaab1302d6797a44d0a22313d9b6f4344cce
Rost911 <rostshulym@gmail.com>
2026-02-03 21:03:38 +0100

    Практикуемся в написании API автотестов. Часть 1

--- tools/assertions/base.py
+++ tools/assertions/base.py
@@ -1,5 +1,7 @@
 from typing import Any
+from typing import Any, Sized
 from api_client_get_user import get_user_response, create_user_response
+
 def assert_status_code(actual: int, expected: int):
     """
     Проверяет, что фактический статус-код ответа соответствует ожидаемому.
@@ -41,4 +43,19 @@ def assert_is_true(actual: Any, name: str):
     assert actual, (
         f'Incorrect value: "{name}". '
         f'Expected true value but got: {actual}'
+    )
+
+def assert_length(actual: Sized, expected: Sized, name: str):
+    """
+    Проверяет, что длины двух объектов совпадают.
+
+    :param name: Название проверяемого объекта.
+    :param actual: Фактический объект.
+    :param expected: Ожидаемый объект.
+    :raises AssertionError: Если длины не совпадают.
+    """
+    assert len(actual) == len(expected), (
+        f'Incorrect object length: "{name}". '
+        f'Expected length: {len(expected)}. '
+        f'Actual length: {len(actual)}'
     )
