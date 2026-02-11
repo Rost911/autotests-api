68627d336447faab5b6b5df6a565a7d8fcbb0b0d
Rost911 <rostshulym@gmail.com>
2026-02-11 21:23:37 +0100

    Практика написания API автотестов для работы с заданиями. Обновление задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -2,11 +2,14 @@ from http import HTTPStatus
 
 import pytest
 from clients.exercises.exercises_schema import (CreateExerciseRequestSchema,
-                                                CreateExerciseResponseSchema, GetExerciseResponseSchema)
+                                                CreateExerciseResponseSchema, GetExerciseResponseSchema,
+                                                UpdateExerciseRequestSchema, UpdateExerciseResponseSchema)
 from fixtures.courses import CourseFixture
 from clients.exercises.exercises_client import ExercisesClient
 from tools.assertions.base import assert_status_code
-from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response
+from tools.assertions.exercises import (assert_create_exercise_response,
+                                        assert_get_exercise_response,
+                                        assert_update_exercise_response)
 from tools.assertions.schema import validate_json_schema
 from fixtures.exercises import function_exercise, ExerciseFixture
 
@@ -50,3 +53,24 @@ class TestExercises:
                                      create_exercise_response=function_exercise.response)
         validate_json_schema(response.json(), response_data.model_json_schema())
 
+
+    def test_update_exercise(self, function_exercise: ExerciseFixture, exercises_client: ExercisesClient):
+        """
+        Проверяет обновление упражнения через PATCH /api/v1/exercises/{exercise_id}.
+
+        Шаги:
+        - формирует запрос на обновление упражнения
+        - отправляет PATCH-запрос с использованием ExercisesClient
+        - проверяет, что статус-код ответа равен 200 OK
+        - проверяет, что данные в ответе соответствуют переданным в запросе
+        - валидирует JSON schema ответа
+        """
+        request = UpdateExerciseRequestSchema()
+
+        response = exercises_client.update_exercise_api(function_exercise.response.exercise.id, request)
+        response_data = UpdateExerciseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_update_exercise_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
